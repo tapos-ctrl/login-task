441fef0605af218790a95c384f61525532561897
tapos-ctrl <taposchandro231@gmail.com>
2021-08-14 13:16:16 +0600

    deshbord

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Deshbord from './components/Deshbord/Deshbord';
 import SignIn from './components/SignIn/SignIn';
 
 function App() {
   return (
     <div className="App">
       <SignIn></SignIn>
+      <Deshbord></Deshbord>
     </div>
   );
 }
---
+++ src/components/Deshbord/Deshbord.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import './Deshbord.css';
+
+const Deshbord = () => {
+    return (
+        <div className="deshbord">
+            <div className="deshbord-title">
+                <h1>Vidyayan</h1>
+            </div>
+            <section className="row deshbord ">
+                <div className="manu-bar">
+                    <ul>
+                        <li><a href="#">All Tutor</a></li>
+
+                        <li><a href="#">Requested Request</a></li>
+                        <li><a href="#">My tutor</a></li>
+                    </ul>
+                </div>
+
+                <div className="deshbord-cart">
+                    <h5>All Tutors</h5>
+                    <div className="search-box-1">
+                        <h6 className="box-title">Search</h6>
+                        <table class="table-size">
+                            <tbody>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+
+                                <tr>
+                                    <th className="table-data" scope="col"></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn sent-btn">Warning</button>
+                    </div>
+                    <div className="search-box-2">
+                        <h6 className="box-title">Search</h6>
+                        <table class="table-size">
+                            <tbody>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+
+                                <tr>
+                                    <th className="table-data" scope="col"></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn sent-btn">Warning</button>
+                   </div>
+
+
+                   <h5>All Tutors</h5>
+                    <div className="search-box-1">
+                        <h6 className="box-title">Search</h6>
+                        <table class="table-size">
+                            <tbody>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+
+                                <tr>
+                                    <th className="table-data" scope="col"></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn sent-btn">Warning</button>
+                    </div>
+                    <div className="search-box-2">
+                        <h6 className="box-title">Search</h6>
+                        <table class="table-size">
+                            <tbody>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+
+                                <tr>
+                                    <th className="table-data" scope="col"></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn sent-btn">Warning</button>
+                   </div>
+
+                   
+                   <h5>All Tutors</h5>
+                    <div className="search-box-1">
+                        <h6 className="box-title">Search</h6>
+                        <table class="table-size">
+                            <tbody>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+
+                                <tr>
+                                    <th className="table-data" scope="col"></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn sent-btn">Warning</button>
+                    </div>
+                    <div className="search-box-2">
+                        <h6 className="box-title">Search</h6>
+                        <table class="table-size">
+                            <tbody>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+                                <tr>
+                                    <th className="table-data" scope="col">Highest Qualification - B . Tech</th>
+                                </tr>
+
+                                <tr>
+                                    <th className="table-data" scope="col"></th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <button type="button" class="btn sent-btn">Warning</button>
+                   </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Deshbord;
